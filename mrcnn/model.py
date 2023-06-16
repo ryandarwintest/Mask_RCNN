ebb4cbc5cf75eae3b496d5bcd2633ae010f6de36
ryandarwintest <133025373+ryandarwintest@users.noreply.github.com>
2023-06-16 15:11:51 -0400

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -619,7 +619,7 @@ def detection_targets_graph(proposals, gt_class_ids, gt_boxes, gt_masks, config)
     return rois, roi_gt_class_ids, deltas, masks
 
 
-class DetectionTargetLayer(KE.Layer):
+class DetectionTargetLayer(KE.base_layer.Layer):
     """Subsamples proposals and generates target box refinement, class_ids,
     and masks for each.
 
